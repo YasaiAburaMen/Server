5a615d48a416a9d2146dd7384c9843ef974630f9
r-yanyo <honma.ryosuke@gmail.com>
2017-09-16 22:41:51 +0900

    implement updating user image in DB

--- back/user/routes/users.js
+++ back/user/routes/users.js
@@ -32,23 +32,46 @@ router.post('/',upload.single('image'),function(req, res, next){
   let name = req.body['name'];
   let email = req.body['e-mail'];
   let image_path = file.filename;
-
-  
-  //save user in mongoDB
-  let newUser = new User({
-    name: name,
-    email: email,
-    image_path: image_path
-  })
-  newUser.save((err)=>{
+  let bef_image_path;
+  User.findOne({email: email},(err,findUser)=>{
     if(err){
-      return res.send(err);
+      console.error(err);
+      res.send(err);
     }
-    else{
-      console.log('saved user.');
-      return res.send("success");
+    else if(!findUser){ //emailが見つからなかったら
+      //save user in mongoDB
+      let newUser = new User({
+        name: name,
+        email: email,
+        image_path: image_path
+      })
+      newUser.save((err)=>{
+        if(err){
+          return res.send(err);
+        }
+        else{
+          console.log('saved user.');
+          return res.send("success");
+        }
+      });
     }
-  });
+    else{ //emailが既に登録されていたら
+      bef_image_path = findUser.image_path;
+      //update the memo in DB
+      User.update({email: email},
+        {$set: {
+            image_path: image_path
+        }},(err,raw)=>{
+          if(err){
+            return res.send(err);
+          }
+          else{
+            //bef_imagepathとimage_pathの画像をレスポンスする？
+            return res.send("success");
+          }
+        });
+    }
+  })
   
 });
 
